5b4c9a6179519470dae785b853ebe7fd770c3717
JiahaoWu <jiahaodev@163.com>
2020-02-12 00:35:55 +0800

    添加士兵派生类

---
+++ Core/Soldier/Grizzly.cs
@@ -0,0 +1,31 @@
+/****************************************************
+	文件：Grizzly.cs
+	作者：JiahaoWu
+	邮箱: jiahaodev@163.ccom
+	日期：2020/02/11 17:23   	
+	功能：豺狼人
+*****************************************************/
+
+public class Grizzly : BaseSoldier
+{
+
+    public Grizzly()
+    {
+        loadProperties();
+
+        base.createFromPrefab("Prefabs/Soldier", this);
+
+        name = "grizzly";
+    }
+
+    public override void updateLogic()
+    {
+        base.updateLogic();
+    }
+
+    public override void loadProperties()
+    {
+        hp = (Fix64)200;
+    }
+
+}
